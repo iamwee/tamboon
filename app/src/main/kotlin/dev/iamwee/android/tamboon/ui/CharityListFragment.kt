32329dd1280277cf545551eba396cbdae0ae5bc4
iammwee <wathin.v@gmail.com>
2020-06-12 22:52:05 +0700

    Add CharityList UI

---
+++ app/src/main/kotlin/dev/iamwee/android/tamboon/ui/CharityListFragment.kt
@@ -0,0 +1,6 @@
+package dev.iamwee.android.tamboon.ui
+
+import androidx.fragment.app.Fragment
+import dev.iamwee.android.tamboon.R
+
+class CharityListFragment : Fragment(R.layout.fragment_charity_list)
